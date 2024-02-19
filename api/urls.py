218b1f7c0bc9d67b1588fb07dafe7106be4d0762
JATorres-zxc <angelotorres200425@gmail.com>
2024-02-19 22:29:11 +0800

    added updatenote and deletenote views and url

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from api.views import *
 urlpatterns = [
     path('', getRoutes, name='routes'),
     path('notes/', getNotes, name='getnotes'),
+    path('notes/<str:pk>/update/', updateNote, name='updatenote'),
+    path('notes/<str:pk>/delete/', deleteNote, name='deletenote'),
     path('notes/<str:pk>/', getNote, name='getnote'),
 ]
