6dd490912e2b251e7acf3ac055249d29704cb1f3
JATorres-zxc <angelotorres200425@gmail.com>
2024-02-22 15:16:15 +0800

    db connected

--- mynotesprj/settings.py
+++ mynotesprj/settings.py
@@ -84,9 +84,12 @@ WSGI_APPLICATION = 'mynotesprj.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "creds.cnf",
+        },
+        
     }
 }
 
